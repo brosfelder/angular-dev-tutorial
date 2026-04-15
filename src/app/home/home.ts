417196dd49a5598e8221908e1d19a6d90137f591
Brian Rosfelder <brosfelder@outlook.com>
2026-04-14 20:05:52 -0400

    completed housinglocation interface

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {HousingLocation} from '../housing-location/housing-location';
+import {HousingLocationInfo} from '../housinglocation';
 
 @Component({
   selector: 'app-home',
@@ -55,5 +56,17 @@ button {
 `,
 })
 export class Home {
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
 
+  housingLocation: HousingLocationInfo = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 }
+
