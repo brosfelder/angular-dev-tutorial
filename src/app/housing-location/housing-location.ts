6be9cf7ca6c377fb5470ce12023e64dce215c273
Brian Rosfelder <brosfelder@outlook.com>
2026-04-15 19:50:55 -0400

    use for loop to iterate through new test data array

--- src/app/housing-location/housing-location.ts
+++ src/app/housing-location/housing-location.ts
@@ -2,7 +2,18 @@ import {Component, input} from '@angular/core';
 import {HousingLocationInfo} from '../housinglocation';
 @Component({
   selector: 'app-housing-location',
-  template: ` <p>housing-location works!</p> `,
+  template: `
+    <section class="listing">
+      <img
+        class="listing-photo"
+        [src]="housingLocation().photo"
+        alt="Exterior photo of {{ housingLocation().name }}"
+        crossorigin
+      />
+      <h2 class="listing-heading">{{ housingLocation().name }}</h2>
+      <p class="listing-location">{{ housingLocation().city }}, {{ housingLocation().state }}</p>
+    </section>
+  `,
   styleUrls: ['../housing-location.css'],
 })
 export class HousingLocation {
